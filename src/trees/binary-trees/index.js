1e0628ea326707f795ca3437c96527c127f65f29
findsakshat <voteforsakshat@gmail.com>
2024-02-28 23:34:44 +0530

    feat: iterative solution for breadth first search in BST

---
+++ src/trees/binary-trees/index.js
@@ -0,0 +1,85 @@
+export { };
+
+class TreeNode {
+  constructor(data = null, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data) {
+    const newNode = new TreeNode(data);
+
+    if (!this.root) {
+      this.root = newNode;
+      return true;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (data < current.data) {
+        // left side
+        if (current.left === null) {
+          current.left = newNode;
+          return true;
+        }
+        current = current.left;
+      } else if (data > current.data) {
+        // right side
+        if (current.right === null) {
+          current.right = newNode;
+          return true;
+        }
+        current = current.right;
+      } else {
+        // duplicate values
+        return false;
+      }
+    }
+  }
+
+  BFS() {
+    const visitedNodes = [];
+
+    const queue = [this.root];
+
+    while (queue.length) {
+      const visited = queue.pop();
+      visitedNodes.push(visited.data);
+
+      if (visited.left) {
+        queue.unshift(visited.left);
+      }
+
+      if (visited.right) {
+        queue.unshift(visited.right)
+      }
+    }
+
+    return visitedNodes;
+  }
+
+}
+
+function main() {
+  const tree = new BinaryTree();
+
+  tree.insert(5);
+  tree.insert(3);
+  tree.insert(7);
+  tree.insert(6);
+  tree.insert(2);
+  tree.insert(4);
+  tree.insert(8);
+
+  console.log(tree.BFS());
+}
+
+main();
