671d4b0f8f9ec845fca7e1ae09474e4fe6008646
findsakshat <voteforsakshat@gmail.com>
2024-03-06 11:00:05 +0530

    feat: solution for removing duplicates in sorted array (optimal)

--- src/arrays/4-remove-duplicates-from-sorted-array/index.js
+++ src/arrays/4-remove-duplicates-from-sorted-array/index.js
@@ -5,8 +5,24 @@ function removeDuplicates(nums = []) {
   return new Set(nums);
 }
 
-function main() {
+function removeDuplicatesOptimal(nums = []) {
+  let j = 0;
 
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[j] !== nums[i]) {
+      nums[j + 1] = nums[i];
+      j = j + 1;
+    }
+  }
+
+  const result = nums.slice(0, j + 1);
+  return result;
+}
+
+function main() {
+  console.log(removeDuplicatesOptimal([1, 1, 2, 2, 2, 3, 3]));
+  console.log(removeDuplicatesOptimal([2, 2, 2, 2, 2]));
+  console.log(removeDuplicatesOptimal([1, 2, 2, 3, 4, 4, 4, 5, 5]));
 }
 
 main();
